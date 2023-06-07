c135cbc524da62ce64671cbe55a9830d7f66dacb
Pavel Khan <pvelkhantopu@gmail.com>
2023-06-07 23:20:08 +0600

    create user when register is happen

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -25,20 +26,48 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const usersCollection = client.db("summerCamp").collection("users");
+    const classCollection = client.db("summerCamp").collection("classes");
+
+
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+
+      res.send({ token })
+    })
+
+    // user related api
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await usersCollection.findOne(query);
+      // console.log(existingUser);
+      if (existingUser) {
+        return res.send({ message: 'User Already Exists!' })
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
+
+   
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('camp is sitting')
+  res.send('camp is sitting')
 })
 
 app.listen(port, () => {
-    console.log(`summer Camp is sitting on port ${port}`);
+  console.log(`summer Camp is sitting on port ${port}`);
 })
